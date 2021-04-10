44d5bf95d69472b874f3d1427e54c64cd37f085f
Ratson <github@ratson.name>
2021-04-11 00:14:55 +0800

    test: avoid using unstable API

--- os/test.ts
+++ os/test.ts
@@ -1,4 +1,4 @@
-import { exists } from "https://deno.land/std@0.92.0/fs/mod.ts";
+import { exists } from "https://deno.land/std@0.92.0/fs/exists.ts";
 import { assert } from "https://deno.land/std@0.92.0/testing/asserts.ts";
 import { userHomeDir } from "./mod.ts";
 

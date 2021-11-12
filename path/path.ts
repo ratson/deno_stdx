63c22c989148099b8a94e24d70f03427806d3ddb
Ratson <github@ratson.name>
2021-11-12 21:31:43 +0800

    chore(path): make pathMap private

--- path/path.ts
+++ path/path.ts
@@ -14,8 +14,9 @@ export class Path {
     this.#path = pathSegments;
   }
 
-  static pathMap = new Map<string, WeakRef<Path>>();
   static gcModulo = 128
+  static #counter = 0;
+  static #pathMap = new Map<string, WeakRef<Path>>();
 
   static cwd() {
     return Path.from(Deno.cwd());
@@ -23,11 +24,12 @@ export class Path {
 
   static from(...pathSegments: string[]) {
     const k = resolve(...pathSegments);
-    const v = this.pathMap.get(k)?.deref();
+    const m = this.#pathMap
+    const v = m.get(k)?.deref();
     if (v) return v;
 
     const p = new this(...pathSegments);
-    this.pathMap.set(k, new WeakRef(p));
+    m.set(k, new WeakRef(p));
 
     this.#counter += 1;
     if (this.#counter % this.gcModulo === 0) this.gc();
@@ -45,10 +47,8 @@ export class Path {
     return Path.from(p);
   }
 
-  static #counter = 0;
-
   static gc() {
-    const m = this.pathMap;
+    const m = this.#pathMap;
     for (const [k, v] of m.entries()) {
       if (!v.deref()) m.delete(k);
     }

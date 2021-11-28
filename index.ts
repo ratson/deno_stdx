30e88751fdcf65d6c507e92479835bb3def5faf2
Ratson <github@ratson.name>
2021-11-28 15:33:16 +0800

    docs: add index file for documentation navigation

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * For documentation navigation
+ */
+export * as async from "./async/mod.ts";
+export * as collections from "./collections/mod.ts";
+export * as log from "./log/mod.ts";
+export * as math from "./math/mod.ts";
+export * as network from "./network/mod.ts";
+export * as path from "./path/mod.ts";
+export * as subprocess from "./subprocess/mod.ts";
+export * as tags from "./tags/mod.ts";
+export * as testing from "./testing/mod.ts";

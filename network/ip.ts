39e586382d21c009c955e29991c323416bf10c13
Ratson <github@ratson.name>
2022-01-29 15:14:42 +0800

    feat(network): accept custom ip provider

--- network/ip.ts
+++ network/ip.ts
@@ -1,47 +1,85 @@
+import type { PromiseOr } from "../typing/promise.ts";
+
 export class IpNotFoundError extends Error {
-  constructor(_message?: string, init?: ErrorInit) {
-    super("Could not get the public IP address", init);
+  constructor(message?: string, init?: ErrorInit) {
+    super(message ?? "Could not get the public IP address", init);
     this.name = "IpNotFoundError";
   }
 }
 
-type Options = {
+type ProviderOptions = {
   v?: 4 | 6;
-  providers?: Array<keyof typeof providers>;
 };
 
-async function icanhazip(options: Options) {
-  const s = options.v ? `ipv${options.v}.` : "";
-  const res = await fetch(`https://${s}icanhazip.com`);
-  const ip = await res.text();
-  return ip.trimEnd();
+export abstract class IpProvider {
+  static registry = new Map<string, IpProvider>();
+
+  static id = "";
+
+  get id(): string {
+    // @ts-expect-error dynamic-prop
+    return this.constructor.id;
+  }
+
+  constructor() {
+    if (!this.id) throw new Error("id is requried");
+
+    const { registry } = IpProvider;
+    if (registry.has(this.id)) throw new Error(`id "${this.id}" is registered`);
+    registry.set(this.id, this);
+  }
+
+  ip(_options: ProviderOptions): PromiseOr<string> {
+    throw new Error("Not implemented");
+  }
 }
 
-async function ifconfig(_options: Options) {
-  const res = await fetch(`https://ifconfig.co/ip`);
-  const ip = await res.text();
-  return ip.trimEnd();
+class IcanhazipProvider extends IpProvider {
+  static id = "icanhazip" as const;
+
+  async ip(options: ProviderOptions) {
+    const s = options.v ? `ipv${options.v}.` : "";
+    const res = await fetch(`https://${s}icanhazip.com`);
+    const ip = await res.text();
+    return ip.trimEnd();
+  }
 }
 
-async function ipify(options: Options) {
-  const s = options.v === 4 ? "" : 64;
-  const res = await fetch(`https://api${s}.ipify.org?format=text`);
-  return res.text();
+class IfconfigProvider extends IpProvider {
+  static id = "ifconfig" as const;
+
+  async ip() {
+    const res = await fetch(`https://ifconfig.co/ip`);
+    const ip = await res.text();
+    return ip.trimEnd();
+  }
 }
 
-const providers = {
-  icanhazip,
-  ifconfig,
-  ipify,
-} as const;
+class IpifyProvider extends IpProvider {
+  static id = "ipify" as const;
 
-export const defaultProviders = ["ipify", "icanhazip", "ifconfig"] as const;
+  async ip(options: ProviderOptions) {
+    const s = options.v === 4 ? "" : 64;
+    const res = await fetch(`https://api${s}.ipify.org?format=text`);
+    return res.text();
+  }
+}
+
+const providers = [IpifyProvider, IcanhazipProvider, IfconfigProvider] as const;
+
+export const defaultProviders = providers.map((C) => new C());
+
+interface Options extends ProviderOptions {
+  providers?: Array<IpProvider | typeof providers[number]["id"]>;
+}
 
 export async function getPublicIP(options?: Options): Promise<string> {
   const opts = { providers: defaultProviders, ...options } as const;
   for (const k of opts.providers) {
+    const provider = typeof k === "string" ? IpProvider.registry.get(k) : k;
+    if (!provider) continue;
     try {
-      return await providers[k](opts as Options);
+      return await provider.ip(opts);
     } catch {
       continue;
     }

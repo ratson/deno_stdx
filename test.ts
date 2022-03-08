8e861684bbb466d0b94d929b44e58c059c0f9d4b
Ratson <github@ratson.name>
2022-03-09 06:46:15 +0800

    test: increase deps limit

--- test.ts
+++ test.ts
@@ -5,5 +5,5 @@ Deno.test("deps", async () => {
   const s = await output([Deno.execPath(), "info", "index.ts"]);
   const m = s.match(/ (\d+) unique/);
   if (!m) unreachable();
-  assertLessOrEqual(m[1], 80);
+  assertLessOrEqual(m[1], 100);
 });

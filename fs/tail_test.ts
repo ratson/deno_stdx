73aa11ff3ab4eadc2f578e63d00922b3475f4e72
Ratson <github@ratson.name>
2022-03-29 17:20:54 +0800

    test(fs/tail): file not exists

--- fs/tail_test.ts
+++ fs/tail_test.ts
@@ -1,11 +1,11 @@
-import { assertStrictEquals } from "../deps_test.ts";
+import { assertRejects, assertStrictEquals } from "../deps_test.ts";
 import { Path } from "../path/path.ts";
 import { Tail } from "./mod.ts";
 
+const encoder = new TextEncoder();
+
 Deno.test("start", async () => {
-  const tempDir = await Path.makeTempDir();
-  const tempFile = tempDir.joinpath("temp.log");
-  const encoder = new TextEncoder();
+  const tempFile = await Path.makeTempFile();
   const tail = new Tail(tempFile.toString());
 
   await tempFile.ensureFile();
@@ -31,4 +31,11 @@ Deno.test("start", async () => {
   }
 
   assertStrictEquals(c, 10);
+});
+
+Deno.test("file not exists", { sanitizeOps: false }, async () => {
+  const tempFile = await Path.makeTempFile();
+  const tail = new Tail(tempFile.toString());
+
+  assertRejects(() => tail.start().next(), Deno.errors.NotFound);
 });

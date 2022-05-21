a6e477cbe5f806e370e2c4f89c1d26f7a4367b68
Ratson <github@ratson.name>
2022-05-22 07:27:52 +0800

    feat(os): export defaultBrowser()

---
+++ os/default_browser.ts
@@ -0,0 +1,26 @@
+import { osType } from "https://deno.land/std@0.134.0/_util/os.ts";
+import { output } from "../subprocess/mod.ts";
+
+async function linux() {
+  const s = await output([
+    "xdg-mime",
+    "query",
+    "default",
+    "x-scheme-handler/http",
+  ]);
+  const id = s.trim();
+  const name = id.replace(/.desktop$/, "").replaceAll("-", " ");
+
+  return { id, name };
+}
+
+export default () => {
+  switch (osType) {
+    case "linux":
+      return linux();
+    case "darwin":
+    case "windows":
+    default:
+      throw new Error("OS is not supported");
+  }
+};

664dae2ee7e072f82403c4739d04fd5f4e15da12
Ratson <github@ratson.name>
2022-08-06 17:12:05 +0800

    chore: remove index.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { assertLessOrEqual, unreachable } from "./deps_test.ts";
-import { output } from "./subprocess/mod.ts";
-
-Deno.test("deps", async () => {
-  const s = await output([Deno.execPath(), "info", "index.ts"]);
-  const m = s.match(/ (\d+) unique/);
-  if (!m) unreachable();
-  assertLessOrEqual(m[1], 100);
-});

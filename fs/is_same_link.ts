96450f08525976e9d25c52d5220c901ed887f22d
Ratson <github@ratson.name>
2023-07-11 22:05:22 +0800

    fix(fs/is_same_link): make it works on windows

--- fs/is_same_link.ts
+++ fs/is_same_link.ts
@@ -1,4 +1,11 @@
-export async function isSameLink(src: string | URL, dest: string | URL) {
+import { resolve } from "https://deno.land/std@0.193.0/path/mod.ts";
+
+export async function isSameLink(src: string, dest: string) {
+  const p1 = resolve(src);
+  const p2 = resolve(dest);
+
+  if (p1 === p2) return true;
+
   const [file1Info, file2Info] = await Promise.all([
     Deno.lstat(src),
     Deno.lstat(dest),

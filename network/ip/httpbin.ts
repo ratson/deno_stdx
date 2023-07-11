a790f62c600481a0208c2a283955fe793dcc6862
Ratson <github@ratson.name>
2023-07-11 22:29:35 +0800

    fix(network/ip): set content-type to json for httpbin request

--- network/ip/httpbin.ts
+++ network/ip/httpbin.ts
@@ -1,7 +1,13 @@
 export const url = "https://httpbin.org/ip";
 
 export default async function (options?: Parameters<typeof fetch>[1]) {
-  const res = await fetch(url, options);
+  const res = await fetch(url, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      "Content-Type": "application/json",
+    },
+  });
   const data: { origin: string } = await res.json();
   return data.origin;
 }
